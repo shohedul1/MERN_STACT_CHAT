a4a2f199487e355dcc96ce52923e5e9783103818
Jane Doe <jane.doe@example.com>
2024-08-12 20:53:15 +0600

    shohiudl5

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,8 @@ import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
 
 
 const app = express();
@@ -13,6 +15,9 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:5173', // Replace with your frontend URL
+}));
 
 
 app.get("/", (req, res) => {
